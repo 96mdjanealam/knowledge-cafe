915d6082bc1a40c117efae3d1a469f6d7dafd3cf
96mdjanealam <arifayeasmin2017@gmail.com>
2024-10-24 16:12:26 +0600

    icons added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,11 @@ import profile from "../../assets/profile.png"
 const Header = () => {
     return (
         <div className="w-11/12 mx-auto flex justify-between items-center py-4 border-b-2">
-            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <div className="flex flex-row items-center gap-2">
+                <img className="w-12" src="knowledge.png" alt="" />
+            <h1 className='text-4xl font-bold'> Knowledge Cafe</h1>
+            </div>
+            
             <img src={profile} alt="" />
         </div>
     );
